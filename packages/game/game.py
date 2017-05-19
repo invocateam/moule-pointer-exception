3720eb245e31df191624f17af6666da7a1ef4456
EnzDev <enzo@mallard.ml>
2017-05-19 17:16:13 +0200

    Base Game

--- packages/game/game.py
+++ packages/game/game.py
@@ -1,7 +1,8 @@
-import board;
-import player;
+from packages.game.board import Board
+from packages.game.player import Player
 
 class Game:
     def __init__(self, inputstring):
-        self.player = Player()
-        self.board = Board(inputstring)
+        data = inputstring.split("/")
+        self.players = [ Player(i, p.split(",")) for i, p in enumerate(data[2][2::].split("-"))] 
+        self.board = Board(data[:2])
