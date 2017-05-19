2dd86234a671cb10a5561508d586b21bd1c2db0b
Vincent <rabierv@hotmail.fr>
2017-05-19 15:30:04 +0200

    log

--- packages/network/network.py
+++ packages/network/network.py
@@ -1,16 +1,18 @@
 import socket
+from config import *
 
 
 class NetworkInterface:
-    def __init__(self, host, port):
+    def __init__(self):
         self.responseSize = 999999
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((host, port))
+        self.socket.connect((IP_SERVEUR, PORT_SERVEUR))
 
     def action(self, message):
         print("Send --> ", message)
         self.socket.send(message)
         r = self.socket.recv(self.responseSize)
+        print(r, "<-- Response")
         return r
 
     def __del__(self):
