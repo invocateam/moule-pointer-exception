274774f2fc00cf24fa76df938e9a7374d34750ec
ReedShalazi <rems.v1@gmail.com>
2017-05-19 15:10:06 +0200

    +++++++ de constantes ! :D

--- config.py
+++ config.py
@@ -9,12 +9,20 @@ sys.path.append(path_of_here)
 
 # ________________________________ définition des constantes
 
-LIMIT_TIME = '1000'
+# générales
+LIMIT_TIME = 1000
+NOMBRE_DE_TOURS_LIMITE = 1500
+PORT_SERVEUR = 1337
+IP_SERVEUR = 'localhost'
+
+# tuiles
 DUNE = 'D'
 SABLE = 'S'
 FRITE = 'F'
 BIERE = 'B'
 INFRANCHISSABLE = 'X'
+
+# directions
 HAUT = 'N'
 BAS = 'S'
 DROITE = 'E'
